1f73cfebf2c3dacf0cdff0d23a4eef3eb3fb634f
20025732 <20025732@rp.edu.sg>
2021-07-28 14:53:46 +0800

    Create Cluster class

---
+++ C206_CaseStudy/src/Career.java
@@ -0,0 +1,30 @@
+
+public class Career {
+  
+    private int career_id;
+    private String career_name;
+
+    public Career(int career_id, String career_name) {
+        super();
+        this.career_id = career_id;
+        this.career_name = career_name;
+    }
+
+    public int getCareerID() {
+        return career_id;
+    }
+
+    public void setCareerID(int career_id) {
+        this.career_id = career_id;
+    }
+
+    public String getCareerName() {
+        return career_name;
+    }
+
+    public void setCareerName(String career_name) {
+        this.career_name = career_name;
+    }
+
+}
+
